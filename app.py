09b1a6a4fc2f4df236d711b280a24766fdf3f298
TomSagi1 <93433824+TomSagi1@users.noreply.github.com>
2021-12-18 19:11:31 +0200

    Updated to assignment 8

--- app.py
+++ app.py
@@ -1,23 +1,41 @@
-from flask import Flask , redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'This is where we begin'
+    return render_template("cv.html")
+
+
+@app.route('/contact')
+def catalog_page():
+    return render_template("cv1.html")
+
+
+@app.route('/assignment8')
+def assignment8():
+    name = "Tom Sagi"
+    hobbies = ["sports", "reading", "cooking", "surfing"]
+    return render_template("assignment8.html", name=name, hobbies=hobbies)
+
 
 @app.route('/about')
 def about_page():
     return redirect('/')
 
+
 @app.route('/catalog')
 def catalog_page():
     return 'This will be a catalog page '
+
 
 @app.route('/transfer')
 def urldirect_page():
     return redirect(url_for('catalog_page'))
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
