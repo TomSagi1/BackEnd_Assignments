abfc1eb321d8d4f0ce4d20ddcd3bdf13919fe886
TomSagi1 <93433824+TomSagi1@users.noreply.github.com>
2021-12-25 20:56:16 +0200

    Updated to Assignment9

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, redirect, url_for, render_template
+from flask import request, session
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 
 @app.route('/')
@@ -20,6 +22,35 @@ def assignment8():
     return render_template("assignment8.html", name=name, hobbies=hobbies)
 
 
+@app.route('/assignment9', methods=['GET', 'POST'])
+def assignment9():
+    search = ''
+    user_name = ''
+    users = {{'user_name': 'tomsag', 'email': 'tomsag@post.bgu.ac.il'},
+             {'user_name': 'superman', 'email': 'superman121@gmail.com'},
+             {'user_name': 'lebronJ', 'email': 'LebronJames@gmail.com'},
+             {'user_name': 'RogerF', 'email': 'rogerf@gmail.com'},
+             {'user_name': 'TheRock', 'email': 'rocking1993@gmail.com'}
+             }
+    method = request.method
+    if method == 'GET':
+        if 'searchIn' in request.args:
+            search = request.args['searchIn']
+            return render_template('assignment9.html', search=search, users=users)
+    if method == 'POST':
+        user_name = request.form['user_name']
+        session['logged_in'] = True
+        session['user_name'] = user_name
+    return render_template('assignment9.html', request_method=request.method, user_name=user_name)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session['logged_in'] = False
+    session['user_name'] = ''
+    return redirect(url_for('assignment9'))
+
+
 @app.route('/about')
 def about_page():
     return redirect('/')
@@ -37,5 +68,3 @@ def urldirect_page():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
