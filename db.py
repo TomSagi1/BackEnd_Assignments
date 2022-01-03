f84d0aa6decbcaa6f43bca241692a7bd99e2de1d
TomSagi1 <93433824+TomSagi1@users.noreply.github.com>
2022-01-03 14:30:44 +0200

    Updated to Assignment10

---
+++ db.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+
+# - DATABASE CONNECTION -
+
+def db_Assignment10(query, query_type: str):
+    return_value = False
+    connection = mysql.connector.connect(host='localhost',
+                                         user='root',
+                                         passwd='root',
+                                         database='assignment10')
+    cursor = connection.cursor(named_tuple=True)
+    cursor.execute(query)
+
+    if query_type == 'commit':
+        connection.commit()
+        return_value = True
+
+    if query_type == 'fetch':
+        query_result = cursor.fetchall()
+        return_value = query_result
+
+    connection.close()
+    cursor.close()
+    return return_value
